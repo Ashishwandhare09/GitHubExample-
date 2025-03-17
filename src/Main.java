54e4c22f4d3fc356956fc102ce1cdb6cd43b457b
Ashishwandhare09 <153617414+Ashishwandhare09@users.noreply.github.com>
2025-03-17 17:55:00 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!This is Updated");
+        System.out.printf("Hello and welcome!This is d");
 
 
         }
